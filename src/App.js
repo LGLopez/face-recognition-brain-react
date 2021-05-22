08c8973aeb92d2baf9783286137fca543ba87a43
Luis Lopez <lglvluislopez@gmail.com>
2021-05-21 21:19:42 -0500

    Parte 296 completada user profile update

--- src/App.js
+++ src/App.js
@@ -72,6 +72,20 @@ class App extends Component{
     // La llamada al modelo de Clarifai
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response => {
+        if (response) {
+          fetch('http://localhost:3020/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/JSON'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => this.setState(
+              Object.assign(this.state.user, { entries: count })
+              )
+            )
+        }
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch(err => console.log(err));
